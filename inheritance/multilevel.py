ace249ef2ec8b2985582de56eb4d3473146bd25d
Harshit joshi <harshjoshi25022002@gmail.com>
2026-05-23 10:37:51 +0530

    multiinheri

--- inheritance/multilevel.py
+++ inheritance/multilevel.py
@@ -1,12 +1,21 @@
 class Employee:
+    def __init__(self):
+        print("i am the constructor of employee")
     a=1
 class Programmer(Employee):
-    b=2
+     def __init__(self):
+        print("i am the constructor of Programmer")
+     b=2
 
 class Manager(Programmer):
-    c=3
+     def __init__(self):
+        super().__init__()
+        print("i am the constructor of Manager")
 
-o=Employee()
+     c=3
+
+
+# o=Employee()
 # print(o.a)
 m=Manager()
 print(m.c)
